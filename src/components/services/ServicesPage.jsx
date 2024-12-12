27b04c3e2ac349558003c3708d252a27a761d466
BrokenHead <kostin.jobbh@gmail.com>
2024-12-12 21:27:57 +0300

    #front: Реализована изоляция стилей для ServicePage

--- src/components/services/ServicesPage.jsx
+++ src/components/services/ServicesPage.jsx
@@ -1,21 +1,23 @@
-import React, {useEffect, useState} from "react";
-import axios from 'axios'
-import './Services.css'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "./Services.module.css";
 
 const ServicesPage = () => {
-
     const [orders, setOrders] = useState([]);
 
     const getAllOrders = async () => {
-        const game = {title: "Valorant"};
-        const requestData = {game, pageNumber: 1, pageSize: 10};
+        const game = { title: "Valorant" };
+        const requestData = { game, pageNumber: 1, pageSize: 10 };
 
         try {
-            const response = await axios.post('http://localhost/orders/getAllOrders', requestData, {withCredentials: true});
+            const response = await axios.post(
+                "http://localhost/orders/getAllOrders",
+                requestData,
+                { withCredentials: true }
+            );
             setOrders(response.data.baseOrder); // Сохраняем полученные заказы в состоянии
         } catch (err) {
-            console.error('Ошибка при получении заказов:', err);
+            console.error("Ошибка при получении заказов:", err);
         }
     };
 
@@ -25,15 +27,15 @@ const ServicesPage = () => {
 
     return (
         <main>
-            <div className="game-header">
+            <div className={styles.gameHeader}>
                 <h1>Valorant Boosting Services</h1>
                 <p>Повышайте свой ранг и улучшайте навыки с нашими профессиональными услугами.</p>
             </div>
-            <section className="services-container ">
+            <section className={styles.servicesContainer}>
                 {orders.length > 0 ? (
                     orders.map((order) => (
-                        <div key={order.id} className="card">
-                            <img src={order.imageUrl} alt={order.title}/>
+                        <div key={order.id} className={styles.card}>
+                            <img src={order.imageUrl} alt={order.title} className={styles.cardImage} />
                             <h3>{order.title}</h3>
                             <p>{order.description}</p>
                             <p>Цена: ${order.basePrice}</p>
@@ -44,7 +46,7 @@ const ServicesPage = () => {
                 )}
             </section>
         </main>
-    )
-}
+    );
+};
 
 export default ServicesPage;
